a871954f967040dc0d6b2b0d15713ed4fa5b3b42
Michael Yu <zhoyu@microsoft.com>
2016-08-08 19:10:45 +0800

    add main activity

---
+++ app/src/main/java/com/michaelyzm/daddytale/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.michaelyzm.daddytale.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhoyu on 8/8/2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
